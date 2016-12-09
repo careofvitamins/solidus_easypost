3b6db4a0e3315edf955be0884346a70885c0bec8
Eric Gross <github@ericgross.com>
2016-12-09 13:52:51 -0500

    Add logging

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -26,6 +26,7 @@ module Spree
       easypost_shipment.buy(rate)
       self.tracking = easypost_shipment.tracking_code
       self.shipping_duration_days = delivery_days_for_selected_rate
+      Rails.logger.info "Bought EasyPost shipment with tracking number #{tracking} and shipping duration of #{shipping_duration_days} days"
     end
 
     private
