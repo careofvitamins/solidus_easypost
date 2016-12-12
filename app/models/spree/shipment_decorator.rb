c83fa79e4843140327ca87eb5a7de5665e41a972
Eric Gross <github@ericgross.com>
2016-12-12 12:32:03 -0500

    Log est delivery date

--- app/models/spree/shipment_decorator.rb
+++ app/models/spree/shipment_decorator.rb
@@ -19,14 +19,20 @@ module Spree
     def buy_easypost_rate
       return if easypost_shipment.postage_label
 
-      rate = easypost_shipment.rates.find do |rate|
+      selected_rate = easypost_shipment.rates.find do |rate|
         rate.id == selected_easy_post_rate_id
       end
 
-      easypost_shipment.buy(rate)
+      easypost_shipment.buy(selected_rate)
+      update_local_attributes
+      Rails.logger.info "Bought EasyPost shipment with tracking number #{tracking}, " \
+"shipping duration: #{shipping_duration_days} days, estimated delivery date #{estimated_delivery_on}"
+    end
+
+    def update_local_attributes
       self.tracking = easypost_shipment.tracking_code
       self.shipping_duration_days = delivery_days_for_selected_rate
-      Rails.logger.info "Bought EasyPost shipment with tracking number #{tracking}, shipping duration: #{shipping_duration_days} days"
+      self.estimated_delivery_on = easypost_shipment.tracker.est_delivery_date
     end
 
     private
@@ -53,7 +59,10 @@ module Spree
       value = easypost_shipment.selected_rate.delivery_days
       return value if value
 
-      Rails.logger.error "Did not get delivery_days for selected shipping rate. Shipment: #{easypost_shipment}, defaulting to #{default_shipping_days}" unless value
+      unless value
+        Rails.logger.error 'Did not get delivery_days for selected shipping rate. '\
+"Shipment: #{easypost_shipment}, defaulting to #{default_shipping_days}"
+      end
       nil
     end
   end
