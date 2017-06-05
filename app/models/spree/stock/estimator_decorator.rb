6c62a9ca8cd7d1e610a61fa48b17ee4b5c78f50b
Eric Gross <github@ericgross.com>
2017-06-05 10:42:02 -0400

    Upgrade for Solidus 2.2.1

--- app/models/spree/stock/estimator_decorator.rb
+++ app/models/spree/stock/estimator_decorator.rb
@@ -90,7 +90,7 @@ module Spree
         method_name = "#{rate.carrier} #{rate.service}"
         Spree::ShippingMethod.find_or_create_by(admin_name: method_name) do |r|
           r.name = method_name
-          r.display_on = :both
+          r.available_to_all = true
           r.code = rate.service
           r.calculator = Spree::Calculator::Shipping::FlatRate.create
           r.shipping_categories = [Spree::ShippingCategory.first]
